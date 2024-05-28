7adad0a9f220e60279bedd069a58aeca434dab8f
Tobias Bednar <me@tobb.dev>
2024-05-28 15:36:19 +0200

    Email validator state machine

---
+++ EmailValidator/program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+string[] emails = {"jan.novak@mensagymnazium.cz", "jan.novak", "jan@.cz"};
+
+bool validateEmail(string input)
+{
+    var email = input.ToCharArray();
+    var emailAllowedChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+-_~!#$%&'./=^'{}|";
+    var domainAllowedChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-";
+    var state = "NAME"; // NAME, AT, DOMAIN, DOT, FAIL
+
+    var hasAtSymbol = false;
+    var hasDotInDomain = false;
+
+    foreach (var c in email)
+    {
+        switch (state)
+        {
+            case "NAME":
+                if (emailAllowedChars.Contains(c))
+                {
+                    state = "NAME";
+                }
+                else if (c == '@')
+                {
+                    state = "AT";
+                    hasAtSymbol = true;
+                }
+                else
+                {
+                    state = "FAIL";
+                }
+                break;
+            case "AT":
+                if (domainAllowedChars.Contains(c))
+                {
+                    state = "DOMAIN";
+                }
+                else
+                {
+                    state = "FAIL";
+                }
+                break;
+            case "DOMAIN":
+                if (domainAllowedChars.Contains(c))
+                {
+                    state = "DOMAIN";
+                }
+                else if (c == '.')
+                {
+                    state = "DOT";
+                    hasDotInDomain = true;
+                }
+                else
+                {
+                    state = "FAIL";
+                }
+                break;
+            case "DOT":
+                if (domainAllowedChars.Contains(c))
+                {
+                    state = "DOMAIN";
+                }
+                else
+                {
+                    state = "FAIL";
+                }
+                break;
+            case "FAIL":
+                return false;
+        }
+    }
+
+    return hasAtSymbol && hasDotInDomain && state == "DOMAIN";
+}
+
+foreach (var email in emails)
+{
+    Console.WriteLine(email);
+    Console.WriteLine(validateEmail(email));
+    Console.WriteLine();
+}
---
+++ EmailValidator/readme.md
@@ -0,0 +1,3 @@
+# Email Validator
+
+[Zadání](https://github.com/mensagymnazium/MgMaturitaIvt2024?tab=readme-ov-file#úloha-1-konečný-automat-pro-validaci-e-mailu)
